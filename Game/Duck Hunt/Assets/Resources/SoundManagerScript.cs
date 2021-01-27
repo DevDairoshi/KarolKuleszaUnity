5daed5fcba8e7f48a41130fe8fae897cfee199b5
DevDairoshi <kar.kul1993@gmail.com>
2021-01-27 11:14:28 +0100

    game commit

---
+++ Game/Duck Hunt/Assets/Resources/SoundManagerScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip StepSound, ShootSound, HitSound, AmbienceSounds;
+    public static AudioSource AudioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StepSound = Resources.Load<AudioClip>("step");
+        ShootSound = Resources.Load<AudioClip>("shoot");
+        HitSound = Resources.Load<AudioClip>("hit");
+        AmbienceSounds = Resources.Load<AudioClip>("ambience");
+
+        AudioSrc = GetComponent<AudioSource>();
+        AudioSrc.rolloffMode = AudioRolloffMode.Linear;
+        AudioSrc.maxDistance = 5.0f;
+        AudioSrc.minDistance = 4.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void Play(string clip)
+    {
+        switch (clip)
+        {
+            case "step":
+            {
+                    AudioSrc.PlayOneShot(StepSound);
+                    break;
+            }
+            case "shoot":
+            {
+                AudioSrc.PlayOneShot(ShootSound);
+                break;
+            }
+            case "hit":
+            {
+                AudioSrc.PlayOneShot(HitSound);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Game/Duck Hunt/Assets/Script/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform PlayerCamera = null;
+    public CharacterController Controller = null;
+    public GameObject Hand = null;
+    public float MouseSensitivity { get; set; }
+    public float CameraPitch { get; set; }
+    public float WalkSpeed { get; set; }
+    public float Gravity { get; set; }
+    public float VelocityY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+
+        MouseSensitivity = 3.5f;
+        CameraPitch = 0.0f;
+        WalkSpeed = 12.0f;
+        Gravity = -16.0f;
+        VelocityY = 0.0f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseLook();
+        UpdateMovement();
+    }
+
+    // My functions
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        CameraPitch -= mouseDelta.y * MouseSensitivity;
+        CameraPitch = Mathf.Clamp(CameraPitch, -80.0f, 45.0f);
+
+        PlayerCamera.localEulerAngles = Vector3.right * CameraPitch;
+        transform.Rotate(Vector3.up * mouseDelta.x * MouseSensitivity);
+    }
+
+    void UpdateMovement()
+    {
+        Vector2 inputDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        inputDirection.Normalize();
+
+        if (Controller.isGrounded)
+        {
+          VelocityY = 0.0f;
+        }
+
+        VelocityY += Gravity * Time.deltaTime;
+        
+        Vector3 velocity = WalkSpeed * (transform.forward * inputDirection.y + transform.right * inputDirection.x) + Vector3.up * VelocityY;
+        Controller.Move(velocity * Time.deltaTime);
+
+        if (velocity.x != 0.0f && velocity.y != 0.0f)
+        {
+            if (_boobingFLag)
+            {
+                PlayerCamera.position = new Vector3(PlayerCamera.position.x, PlayerCamera.position.y - 0.03f/5, PlayerCamera.position.z);
+                Hand.transform.position = new Vector3(Hand.transform.position.x - 0.003f/5, Hand.transform.position.y - 0.002f/5, Hand.transform.position.z);
+            }
+            else
+            {
+                PlayerCamera.position = new Vector3(PlayerCamera.position.x, PlayerCamera.position.y + 0.03f/5, PlayerCamera.position.z);
+                Hand.transform.position = new Vector3(Hand.transform.position.x + 0.003f/5, Hand.transform.position.y+0.002f/5, Hand.transform.position.z);
+            }
+
+            _boobingTimer += Time.deltaTime;
+            if (_boobingTimer > _boobingTime)
+            {
+                _boobingFLag = !_boobingFLag;
+                _boobingTimer = 0.0f;
+                SoundManagerScript.Play("step");
+            }
+        }
+
+        var posiotion = transform.position;
+        posiotion.x = Mathf.Clamp(transform.position.x, 300, 700);
+        posiotion.z = Mathf.Clamp(transform.position.z, 300, 700);
+        transform.position = posiotion;
+    }
+
+    private bool _boobingFLag = false;
+    private float _boobingTime = 0.5f;
+    private float _boobingTimer;
+}
